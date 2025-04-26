1d79eeb5c61575016829aa6ff5920732437e08f7
GavRaz <gavindiscord01@gmail.com>
2025-04-26 12:49:47 -0500

    hgfghdfjhjhdfhjdfhjk

--- javaprojects/consoleCalc.java
+++ javaprojects/consoleCalc.java
@@ -2,12 +2,40 @@ package javaprojects;
 import java.util.Scanner;
 
 public final class consoleCalc {
-    public static void main(String[] args) {
-        Scanner userInput = new Scanner(System.in);
+    public int a;
+    public int b;
 
-        //TEMPORARY TEST OF SCANNER
-        System.out.println("Input username: ");
-        String user = userInput.nextLine();
-        System.out.println("Username is "+ user);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String modeName = "";
+
+        System.out.println("--CONSOLE CALCULATOR v1 BY GAVRAZ--");
+        System.out.println("MODES: 1-ADDITION, 2-SUBTRACTION, 3-MULTIPLICATION, 4-DIVISION");
+        System.out.println("SELECT MODE: ");
+        int mode = input.nextInt();
+
+        switch (mode) {
+            case 1:
+                modeName = "ADDITION";
+                break;
+            case 2:
+                modeName = "SUBTRACTION";
+                break;
+            case 3:
+                modeName = "MULTIPLICATION";
+                break;
+            case 4:
+                modeName = "DIVISION";
+                break;
+            default:
+                modeName = "INVALID MODE SELECTION";
+                break;
+        }
+
+        System.out.println("MODE SELECTED: "+modeName);
+
+        if (modeName == "INVALID MODE SELECTION") {
+            
+        }
     }
 }

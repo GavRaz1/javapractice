ec517104441e13e1b58dfb68afa518195f0eab28
GavRaz <gavindiscord01@gmail.com>
2025-04-27 12:27:43 -0500

    getting really close plus more organization for future conveinence

---
+++ javaprojects/consolecalculator/calcFunctions.java
@@ -0,0 +1,24 @@
+package javaprojects;
+
+public class calcFunctions {
+    public double a;
+    public double b;
+    public double c;
+
+    public void add(int a, int b, int c) {
+        c = a+b;
+    }
+
+    public void sub(int a, int b, int c) {
+        c = a+b;
+    }
+
+
+    public void mul(int a, int b, int c) {
+        c = a+b;
+    }
+
+    public void div(int a, int b, int c) {
+        c = a+b;
+    }
+}

d0207ddb0bd0ffe69d7e83591801f437cbc02524
GavRaz <gavindiscord01@gmail.com>
2025-04-26 23:04:25 -0500

    days work (gotta fix some bs) 4/26

---
+++ javaprojects/calcFunctions.java
@@ -0,0 +1,11 @@
+package javaprojects;
+
+public class calcFunctions {
+    public int a;
+    public int b;
+    public int c;
+
+    public void add(int a, int b, int c) {
+        c = a+b;
+    }
+}
--- javaprojects/consoleCalc.java
+++ javaprojects/consoleCalc.java
@@ -1,18 +1,22 @@
 package javaprojects;
 import java.util.Scanner;
+import javaprojects.calcFunctions;
 
-public final class consoleCalc {
-    public int a;
-    public int b;
-    
+public class consoleCalc {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        calcFunctions functions = new calcFunctions();
         String modeName = "";
+        int mode;
+        int val1;
+        int val2;
+        int result;
 
         System.out.println("--CONSOLE CALCULATOR v1 BY GAVRAZ--");
         System.out.println("MODES: 1-ADDITION, 2-SUBTRACTION, 3-MULTIPLICATION, 4-DIVISION");
+        System.out.println("NOTE: THIS PROGRAM IS IN VERY EARLY STAGES AND YOU \nCAN ONLY DO ONE CALCULATION BEFORE RESTARTING");
         System.out.println("SELECT MODE: ");
-        int mode = input.nextInt();
+        mode = input.nextInt();
 
         switch (mode) {
             case 1:
@@ -34,5 +38,12 @@ public final class consoleCalc {
 
         System.out.println("MODE SELECTED: "+modeName);
         System.out.println("-----------------------------------");
+        System.out.println("TYPE VALUE A:");
+        val1 = input.nextInt();
+        System.out.println("TYPE VALUE B:");
+        val2 = input.nextInt();
+
+        functions.add(val1,val2,result);
+        System.out.println(result);
     }
 }

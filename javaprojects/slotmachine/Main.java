11235671d47503cffc501a2ea495325d7e221f38
GavRaz <gavindiscord01@gmail.com>
2025-06-21 21:25:57 -0500

    man just rewrite this bullshit you retard

--- javaprojects/slotmachine/Main.java
+++ javaprojects/slotmachine/Main.java
@@ -6,9 +6,10 @@ public class Main {
     public static String[] slotSet = {"o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", 
     "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "t", "t", "t", "t", "t", "Y", "Y", "Y", "X", "X"};
 
-    public double bid = 0.0;
+    private double bid = 0.0;
 
-    public static void main(String[] args) {
+    private class Gamble() {
+        public static void play() {
         Random rnum = new Random();
         Scanner input = new Scanner(System.in);
         String machine = "";
@@ -17,33 +18,23 @@ public class Main {
         char b = ' ';
         char c = ' ';
         double balance = rnum.nextInt(100, 1000);
-        double bid = 0.0;
         
-        info();
+        System.out.println("SLOT MACHINE YOU WILL GAMBLE BY GAVVLE THE RAZZLE\nYOU WILL GET A RANDOM AMOUNT OF MONEY AND YOU MUST GET THREE IN A ROW TO GET YOUR MONEY MULTIPLIED OR LOSE IT ALL");
+        System.out.println("ooo = 0.5x\nccc = 1x\nttt = 2x\nYYY = 4x\nXXX = 10x");
+        System.out.println("-------------------------------------------------------------");
+
         while (true) {
             if (gambling == false) {
-                bet(input, balance, bid);
+                bet(input, balance);
                 gambling = true;
             } else {
-                gamble(rnum, machine, a, b, c, balance, bid);
+                gamble(rnum, machine, a, b, c, balance);
                 gambling = false;
             }
         }
     }
 
-    public static void info() {
-        System.out.println("SLOT MACHINE YOU WILL GAMBLE BY GAVVLE THE RAZZLE\nYOU WILL GET A RANDOM AMOUNT OF MONEY AND YOU MUST GET THREE IN A ROW TO GET YOUR MONEY MULTIPLIED OR LOSE IT ALL");
-        System.out.println("ooo = 0.5x\nccc = 1x\nttt = 2x\nYYY = 4x\nXXX = 10x");
-        System.out.println("-------------------------------------------------------------");
-    }
-
-    public static void bet(Scanner input, double money, double bid) {
-        System.out.println("BALANCE: "+money);
-        System.out.println("HOW MUCH WOULD YOU LIKE TO BID?");
-        bid = bid + input.nextDouble();
-    }
-
-    public static void gamble(Random num, String result, char a, char b, char c, double money, double bid) {
+    private void gamble(Random num, String result, char a, char b, char c, double money) {
         System.out.println(money);
         System.out.println(bid);
         System.out.println(money - bid);
@@ -65,31 +56,31 @@ public class Main {
 
         if (a == b && b == c) {
            if (b == 'o') {
-            money = money + (bid*0.5);
-            bid = 0.0;
+            money = money + (this.bid*0.5);
+            this.bid = 0.0;
             System.out.println("\nYOU WON 0.5X MULTIPLIER!");
             System.out.println("-------------------------------------------------------------");
             
            } else if (b == 'c') {
-            money = money + bid;
+            money = money + this.bid;
             System.out.println("\nYOU WON 1.0X MULTIPLIER!");
             System.out.println("-------------------------------------------------------------");
             
            } else if (b == 't') {
-            money = money + (bid*2);
-            bid = 0.0;
+            money = money + (this.bid*2);
+            this.bid = 0.0;
             System.out.println("\nYOU WON 2.0X MULTIPLIER!");
             System.out.println("-------------------------------------------------------------");
             
            } else if (b == 'Y') {
-            money = money + (bid*4);
-            bid = 0.0;
+            money = money + (this.bid*4);
+            this.bid = 0.0;
             System.out.println("\nYOU WON 4.0X MULTIPLIER!");
             System.out.println("-------------------------------------------------------------");
             
            } else if (b == 'X') {
-            money = money + (bid*10);
-            bid = 0.0;
+            money = money + (this.bid*10);
+            this.bid = 0.0;
             System.out.println("\nYOU WON 10.0X MULTIPLIER!");
             System.out.println("-------------------------------------------------------------");
             
@@ -99,5 +90,10 @@ public class Main {
             System.out.println("-------------------------------------------------------------");
             
         }
+    }
+    }
+
+    public static void main(String[] args) {
+        
     }
 }

4b58fab77c470e643d88c9e6621afd9afa51eecf
GavRaz <gavindiscord01@gmail.com>
2025-06-22 21:57:03 -0500

    SLOT MACHINE V1 COMPLETE

--- javaprojects/slotmachine/slotmachine/Gamble.java
+++ javaprojects/slotmachine/slotmachine/Gamble.java
@@ -1,111 +1,87 @@
 package slotmachine;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
 public class Gamble {
 
-    static final String[] slotSet = {"o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "c", "c", "c", "c", "c", "c", "c", "c", "c", "c", "t", "t", "t", "t", "t", "Y", "Y", "Y", "X", "X"};
+    static final String[] slotSet = {"o","c","t","Y","X"};
+    static char[] activeSlots = {'d','i','h'};
+
     static Random rnum;
     static Scanner input;
     static String result;
     static double balance;
     static double bid;
-    static char a;
-    static char b;
-    static char c;
     static boolean gambling;
     static boolean checkFail;
-    
     
     public Gamble() {
         rnum = new Random();
         input = new Scanner(System.in);
         result = "";
-        balance = rnum.nextInt(100,1000);
+        balance = balance + rnum.nextInt(100,1000);
         bid = 0.0;
-        a = ' ';
-        b = ' ';
-        c = ' ';
-        gambling = false;
-        checkFail = false;
     }
 
     void check() {
         if (bid > balance) {
             bid = 0.0;
-            System.out.println("BID HIGHER THAN BALANCE, PLEASE REBID");
-            checkFail = true;
+            System.out.println("BID HIGHER THAN BALANCE, RESTART PROGRAM");
         }
     }
 
     void bet() {
         System.out.println("HOW MUCH WOULD YOU LIKE TO BID?");
-        bid = bid + input.nextDouble();
+        System.out.println(balance);
+        bid += input.nextDouble();
     }
 
     void gamble() {
-        balance = balance - bid;
+        balance -= bid;
         System.out.println("-------------------------------------------------------------");
-        System.out.println("Rolling...");
+        result = result+"\r|"+slotSet[rnum.nextInt(0,5)]+"|"+slotSet[rnum.nextInt(0,5)]+"|"+slotSet[rnum.nextInt(0,5)]+"|";
+        System.out.println(result);
+        activeSlots[0] = result.charAt(2);
+        activeSlots[1] = result.charAt(4);
+        activeSlots[2] = result.charAt(6);
 
-        try {
-            TimeUnit.SECONDS.sleep(3);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        result = result+"\r|"+slotSet[rnum.nextInt(0,40)]+"|"+slotSet[rnum.nextInt(0,40)]+"|"+slotSet[rnum.nextInt(0,40)]+"|   ";
-        a = result.charAt(2);
-        b = result.charAt(4);
-        c = result.charAt(6);
-
-        if (a == b && b == c) {
-            if (b == 'o') {
+        if (activeSlots[0] == activeSlots[1] && activeSlots[1] == activeSlots[2]) {
+            if (activeSlots[1] == 'o') {
                 balance = balance + (bid*0.5);
                 System.out.println("\nYOU WON 0.5X MULTIPLIER!");
                 System.out.println("-------------------------------------------------------------");
-                bid = 0.0;
-            } else if (b == 'c') {
+                bid -= bid;
+            } else if (activeSlots[1] == 'c') {
                 balance = balance + bid;
                 System.out.println("\nYOU WON 1.0X MULTIPLIER!");
                 System.out.println("-------------------------------------------------------------");
                 bid = 0.0;
-            } else if (b == 't') {
+            } else if (activeSlots[1] == 't') {
                 balance = balance + (bid*2);
                 System.out.println("\nYOU WON 2.0X MULTIPLIER!");
                 System.out.println("-------------------------------------------------------------");
-                bid = 0.0;
-            } else if (b == 'Y') {
+                bid -= bid;
+            } else if (activeSlots[1] == 'Y') {
                 balance = balance + (bid*4);
-                System.out.println("\nYOU WON 4.0X MULTIPLIER!");
+                System.out.println("\nYOU WON 4.0X MULTIPLIER!!");
                 System.out.println("-------------------------------------------------------------");
-                bid = 0.0;
-            } else if (b == 'X') {
+                bid -= bid;
+            } else if (activeSlots[1] == 'X') {
                 balance = balance + (bid*10);
-                System.out.println("\nYOU WON 10.0X MULTIPLIER!");
+                System.out.println("\nYOU WON 10.0X MULTIPLIER!!!");
                 System.out.println("-------------------------------------------------------------");
-                bid = 0.0;
-            } else {
-                System.out.println("\nBUST!");
-                System.out.println("-------------------------------------------------------------");
-                bid = 0.0;
+                bid -= bid;
             }
+        } else {
+            System.out.println("\nBUST!");
+            System.out.println("-------------------------------------------------------------");
+            bid -= bid;
         }
     }
 
     void play() {
-        while (true) {
-            if (gambling == false) {
-                bet();
-                check();
-                if (checkFail == true) {
-                    continue;
-                }
-                gambling = true;
-            } else {
-                gamble();
-            }
-        }
+        bet();
+        check();
+        gamble();
     }
 }
--- javaprojects/slotmachine/slotmachine/Main.java
+++ javaprojects/slotmachine/slotmachine/Main.java
@@ -1,15 +1,22 @@
 package slotmachine;
 
-// import slotmachine.Gamble;
-
 public class Main {
     static Gamble mechanics;
+    static boolean active;
     public static void main(String[] args) {
         mechanics = new Gamble();
+        active = false;
+
         System.out.println("SLOT MACHINE YOU WILL GAMBLE BY GAVVLE THE RAZZLE\nYOU WILL GET A RANDOM AMOUNT OF MONEY AND YOU MUST GET THREE IN A ROW TO GET YOUR MONEY MULTIPLIED OR LOSE IT ALL");
         System.out.println("ooo = 0.5x\nccc = 1x\nttt = 2x\nYYY = 4x\nXXX = 10x");
         System.out.println("-------------------------------------------------------------");
 
-        mechanics.play();
+        while (true) {
+            if (active == false) {
+                active = true;
+                mechanics.play();
+                active = false;
+            }
+        }
     }
 }
